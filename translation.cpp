d502f5925ea2dd9fd67d2e36bc099f52e58f2ff0
Susmita-Priya <82328288+Susmita-Priya@users.noreply.github.com>
2023-07-26 15:24:56 +0600

    Update translation.cpp

--- translation.cpp
+++ translation.cpp
@@ -2,46 +2,36 @@
 
 #include <GL/gl.h>
 #include <GL/glut.h>
-#include <cmath>
 
-void drawsquare(float tx,float ty)
+void drawTriangle()
 {
-    glColor3f(1.0f,0.0f,1.0f);
+    glColor3f(0.0f, 0.0f, 1.0f);
 
-    glBegin(GL_QUADS);
-    glVertex3f(0.02f+tx, 0.02f+ty, 0.0f);
-    glVertex3f(0.02f+tx, 0.35f+ty, 0.0f);
-    glVertex3f(0.5f+tx, 0.35f+ty, 0.0f);
-    glVertex3f(0.5f+tx, 0.02f+ty, 0.0f);
+    glBegin(GL_TRIANGLES);
+    glVertex3f(0.0f, 0.0f, 0.0f);
+    glVertex3f(0.5f, 0.0f, 0.0f);
+    glVertex3f(0.25f, 0.5f, 0.0f);
     glEnd();
-
 }
+
 void display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glColor3f(0.0, 0.0, 1.0);
-
-    glBegin(GL_QUADS);
-    glVertex3f(0.02f, 0.02f, 0.0f);
-    glVertex3f(0.02f, 0.35f, 0.0f);
-    glVertex3f(0.5f, 0.35f, 0.0f);
-    glVertex3f(0.5f, 0.02f, 0.0f);
-    glEnd();
-
-    float tx=0.2f;
-    float ty=0.2f;
+    // Draw the original triangle
+    drawTriangle();
 
     glPushMatrix(); // Save the current transformation matrix
-    glTranslatef(tx, ty, 0.0f); // Apply translation
 
-    // Draw the square
-    drawsquare(tx,ty);
+    // Apply translation
+    glTranslatef(0.5f, 0.5f, 0.0f);
+
+    // Draw the translated triangle
+    drawTriangle();
 
     glPopMatrix(); // Restore the previous transformation matrix
 
     glFlush();
-
 }
 
 void init(void)
@@ -49,7 +39,7 @@ void init(void)
     glClearColor(0.0, 0.0, 0.0, 0.0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(0.0, 1.0, 0.0, 1.0, -10.0, 10.0);
+    glOrtho(0.0, 2.0, 0.0, 2.0, -10.0, 10.0);
 }
 
 int main(int argc, char **argv)
@@ -58,7 +48,7 @@ int main(int argc, char **argv)
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(600, 600);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("Basic shapes");
+    glutCreateWindow("Transformations on Triangle");
     init();
     glutDisplayFunc(display);
     glutMainLoop();
